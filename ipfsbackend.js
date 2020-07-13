a516aabffeec8a448241483e1b3ef32a98872ab0
abhinavmir <atg271@gmail.com>
2020-07-13 21:08:40 +0530

    expressJS

--- ipfsbackend.js
+++ ipfsbackend.js
@@ -1,8 +1,22 @@
 const ipfsAPI = require('ipfs-api');
 const express = require('express');
 const fs = require('fs');
-var path = require('path');
+const path = require('path');
+const firebase = require('firebase')
 
+//firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyBwR7gwCYGV8gzrsACBG63isAf8IJiy2GE",
+    authDomain: "abhi-1234.firebaseapp.com",
+    databaseURL: "https://abhi-1234.firebaseio.com",
+    projectId: "abhi-1234",
+    storageBucket: "abhi-1234.appspot.com",
+    messagingSenderId: "1060909375226",
+    appId: "1:1060909375226:web:e5853fdde27f844cda7343"
+};
+
+firebase.initializeApp(firebaseConfig)
+//writeUserData("test","test");
 
 //declarations
 const app = express();
@@ -14,29 +28,27 @@ app.use(express.urlencoded({
   extended: true
 }))
 
-app.get('/post', function(req, res) {
-    res.sendFile(path.join(__dirname + '/html/post.html'));
-});
-
-
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/html/index.html'));
 });
 
+app.get('/post', function(req, res) {
+    res.sendFile(path.join(__dirname + '/html/post.html'));
+});
+
 app.post('/submit-form', (req, res) => {
   const username = req.body.username
-//  console.log("1/3");
   let testBuffer = new Buffer(username);
-//  console.log("2/3");
   ipfs.files.add(testBuffer, function (err, file) {
-      if (err) {
-        console.log(err);
-      }
+	  
+      if (err) {console.log(err);}
       console.log(file)
 	  const hash = file[0]['hash']
-	  console.log(hash)
-    })
-//  console.log("3/3 Complete!");
+	  firebase.database().ref().set({
+	      title: "title",
+	      hash: "hash"
+	    });
+	})
   res.end()
 })
 
